00157052625ddc033229a598e5cf0441296b6450
StephaneBouret <sbouret@orange.fr>
2020-02-13 16:02:04 +0100

    modif view.php

--- View/View.php
+++ View/View.php
@@ -13,6 +13,23 @@ public function __construct(){
 $this->page = file_get_contents('template/head.html');
 $this->page .= file_get_contents('template/nav.html');
 
+if(!isset($_GET['controller'])){
+    $_GET['controller'] = "ticket";
+}
+
+if ($_GET['controller'] == "ticket") {
+    $this->page = str_replace('{activeTic}','active',$this->page);
+}
+if ($_GET['controller'] == "actions"){
+    $this->page = str_replace('{activeAct}','active',$this->page);
+}
+if ($_GET['controller'] == "archives"){
+    $this->page = str_replace('{activeArc}','active',$this->page);
+}
+if ($_GET['controller'] == "technicians"){
+    $this->page = str_replace('{activeInt}','active',$this->page);
+}
+
 }
 
 
