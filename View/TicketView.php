464e2b233452541383070e9934641eef6c37b89d
StephaneBouret <sbouret@orange.fr>
2020-02-13 16:48:11 +0100

    modif TicketView.php

--- View/TicketView.php
+++ View/TicketView.php
@@ -10,26 +10,44 @@ class TicketView extends View {
      * @return void
      */
     public function displayHome($listTickets) {
-        var_dump($listTickets);
+        // var_dump($listTickets);
             $this->page .="<h1 class='text-center'>LES DEMANDES DE CONTACTS !</h1>";
-            $this->page .= "<table class='table text-center col p-2 mt-4 mb-4'>";
-            $this->page .= "<thead class='thead-light'><th>Date</th><th>Nom</th><th>Prénom</th><th>E-Mail</th><th>Sujet</th><th>Message</th><th>Voir</th><th>Statut</th></thead>";
-         /*   foreach ($listUsers as $users) {
-                $this->page .= "<tr><td>" .$users['username'] 
-                ."</td><td class=''>" .$users['password']
-                ."</td><td class=''>" .$users['firstname']
-                ."</td><td class=''>" .$users['lastname']
-                ."</td><td><a class='btn btn-danger' href='index.php?controller=user&action=suppDB&id="
-                .$users['id']
-                ."'><i class='fas fa-trash-alt'></i></a></td><td><a class='btn btn-primary' href='index.php?controller=user&action=updateForm&id="
-                .$users['id']
-            ."'><i class='fas fa-edit'></i></a></td></tr>";
+            foreach ($listTickets as $ticket) {
+                $newDate = date("d-m-Y H:i:s", strtotime($ticket['date_debut']));
+                // VERSION DESKSTOP
+                $this->page .= '<div class="card d-none d-md-block mt-4 mb-4 ">
+                <div class="card-header bg-success text-white"><h5>'
+                . $newDate ." " .$ticket['prenom'] ." " .$ticket['nom']
+                .'</h5></div>
+                <div class="card-body">
+                    <div class="d-flex"> 
+                    <h5 class="card-title">Sujet : ' . $ticket['sujet'] .'</h5>';
+                    $actif = "<p class='text-center col-1 mt-1 text-success'><i class='fas fa-circle'></i></p>";
+                    if ($ticket['id_linku_statut'] == 1) {
+                        $actif = "<p class='text-center col-1 mt-1 text-danger'><i class='fas fa-circle'></i></p>";
+                    } elseif ($ticket['id_linku_statut'] == 2) {
+                        $actif = "<p class='text-center col-1 mt-1 text-warning'><i class='fas fa-circle'></i></p>";
+                    }
+                    $this->page .= $actif;
+                    $this->page .= '</div>
+                    <p class="card-text">'.mb_strimwidth($ticket['desc_ticket'], 0, 60, "[...]").'</p>
+                    <a href="#" class="btn btn-warning mr-4"><i class="fas fa-eye"></i></a>
+                </div></div>';
+                // VERSION TABLETTE / SMARTPHONE
+                $this->page .= '<div class="card d-sm-block d-md-none bg-success text-white mt-4 mb-4">
+                <div class="card-header"><h4>'
+                .strftime(" %d %m %G", strtotime($ticket['date_debut'])) ."</h4> " .$ticket['prenom'] ." " .$ticket['nom']
+                .'</div>
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-warning col-1 col-md-4 mr-4"><i class="fas fa-eye"></i></a>
+                    <a href="#" class="btn btn-primary col-3">Go somewhere</a>
+                </div>
+                </div>'; 
             }
-        */
-            $this->page .= "</table>";
            // $this->page .= file_get_contents('template/detail.html');
             $this->displayPage();
-    }
-
-
+        }
+    
 }
